0147aefc1871a329c557751314f615775f4d788b
NataliaRojas <natyrojas134@gmail.com>
2024-04-21 15:39:29 -0500

    Mensaje descriptivo de commit

---
+++ app.py/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+registros = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        tarea = request.form['tarea']
+        estado = request.form['estado']
+        nuevo_registro = {'tarea': tarea, 'estado': estado}
+        registros.append(nuevo_registro)
+        return redirect(url_for('index'))
+        
+    else:
+        return render_template('index.html', registros=registros)
+
+@app.route('/actualizar_estado/<tarea>', methods=['POST'])
+def actualizar_estado(tarea):
+    nuevo_estado = request.form['estado']
+    for registro in registros:
+        if registro['tarea'] == tarea:
+            registro['estado'] = nuevo_estado
+            break
+    return redirect(url_for('index'))
+
+@app.route('/eliminar_tarea/<tarea>', methods=['POST'])
+def eliminar_tarea(tarea):
+    for registro in registros:
+        if registro['tarea'] == tarea:
+            registros.remove(registro)
+            break
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
